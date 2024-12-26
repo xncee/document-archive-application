05afa72556f340095362b1b1997142fe82b4c460
Saif <ro1c@yahoo.com>
2024-12-27 00:57:55 +0300

    test commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,12 +4,11 @@ module zarqa.governmentproject {
     requires java.sql;
     requires io.github.cdimascio.dotenv.java;
     requires opencv;
-    requires tess4j;
+    requires java.desktop;
+    requires com.google.protobuf;
+    requires org.apache.poi.ooxml;
     requires kernel;
     requires layout;
-    requires org.apache.poi.poi;
-    requires org.apache.poi.ooxml;
-    requires java.desktop;
 
 
     opens view to javafx.fxml;
