9ba6158fffe5ce2189e21565b5d38a9e38bf191b
Saif <ro1c@yahoo.com>
2024-12-28 08:19:05 +0300

    adjustment to dashboard page

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("signin-view.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("results-filter-view.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         stage.setScene(scene);
