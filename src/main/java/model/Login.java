d76c53f5c1cc6cc2ba0ed869b73a85e353518b26
MOD-999 <mohammadqatanani83@outlook.com>
2024-12-29 11:30:02 +0300

    implemented sigIn & signUp methods

--- src/main/java/model/Login.java
+++ src/main/java/model/Login.java
@@ -1,6 +1,6 @@
 package model;
 
-import database.DBFacade;
+import model.database.DBFacade;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -16,28 +16,24 @@ public class Login {
     public boolean signIn(String username, String password) {
         try {
             return dbFacade.authUser(username, password);
-        } catch (SQLException e) {
-            e.printStackTrace();
+        }
+        catch (SQLException e) {
+            //e.printStackTrace();
             return false;
         }
     }
 
     public boolean signUp(String username, String email, String fullName, String password) {
         try {
-            if (isUsernameAvailable(username)) {
+            if (!isUsernameAvailable(username)) {
                 return false;
             }
-            Map<String, Object> userValues = Map.of(
-                    "username", username,
-                    "email", email,
-                    "full_name", fullName,
-                    "password", password
-            );
-            int rowsAffected = dbFacade.getDbManager().insert("users", userValues);
-            return rowsAffected > 0;
 
-        } catch (SQLException e) {
-//          e.printStackTrace();
+            return dbFacade.addUser(username, email, password, fullName);
+
+        }
+        catch (SQLException e) {
+            //e.printStackTrace();
             return false;
         }
     }
@@ -46,14 +42,5 @@ public class Login {
     public boolean isUsernameAvailable(String username) {
         List<Map<String, String>> userData = dbFacade.searchUsers(username, true, "username");
         return userData.isEmpty();
-    }
-
-    public boolean isUserPasswordAvailable(String username, String password) {
-        List<Map<String, String>> userData = dbFacade.searchUsers(username, true, "username");
-        if (userData.isEmpty()) {
-            return false;
-        }
-        String storedPassword = userData.get(0).get("password");
-        return storedPassword.equals(password);
     }
 }
