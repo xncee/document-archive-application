36d3d716245d5da54892af948a3eb434fbf6847b
Saif <ro1c@yahoo.com>
2024-12-29 03:29:47 +0300

    initial

--- src/main/java/model/Login.java
+++ src/main/java/model/Login.java
@@ -1,7 +1,11 @@
 package model;
 
 public class Login {
-    // validate fields
-    // authenticateUser(username, password)
-    // +signUp()
+    // validate fields first
+    public boolean signIn(String username, String password) {
+        return true;
+    }
+    public boolean signUp(String username, String password, String email, String name) {
+        return true;
+    }
 }
