5e31afe6d75cbf29e9f15d75666244db495cf24e
Saif <ro1c@yahoo.com>
2024-12-31 20:13:02 +0300

    moved

--- src/main/java/services/ActivityLogger.java
+++ src/main/java/services/ActivityLogger.java
@@ -1,6 +1,8 @@
-package controllers;
+package services;
 
 import data.DBFacade;
+import utils.ErrorHandler;
+
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 
@@ -9,19 +11,11 @@ public class ActivityLogger {
     private void commentLog(String documentId, int userId,String comment){
         LocalDateTime localDateTime = LocalDateTime.now();
         if (comment != null && !comment.isEmpty()) {
-            try {
-                dbfacade.addActivity(documentId,userId,"Added a comment: "+comment, localDateTime);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+            dbfacade.addActivity(documentId,userId,"Added a comment: "+comment, localDateTime);
         }
     }
     private void statusLog(String documentId, int userId,String newStatus){
         LocalDateTime localDateTime = LocalDateTime.now();
-        try {
-            dbfacade.addActivity(documentId,userId,"Updated status to "+newStatus, localDateTime);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        dbfacade.addActivity(documentId,userId,"Updated status to "+newStatus, localDateTime);
     }
 }
