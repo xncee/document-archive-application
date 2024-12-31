40e7ff735dd7c252f702ccaf14bfa49c2c01d8c3
Saif <ro1c@yahoo.com>
2024-12-31 20:18:18 +0300

    fields services

--- src/main/java/services/FieldsServices.java
+++ src/main/java/services/FieldsServices.java
@@ -6,22 +6,17 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class FieldsServices {
-
-    // Method to set the field as valid
-
     public static void setRedBorder(Node element) {
         element.setStyle("-fx-border-color: red");
     }
-    public static void resetBorderColor(Node element) {
+    private static void resetBorderColor(Node element) {
         element.setStyle("-fx-border-color: #d4d4d4");
     }
     public static void setFieldValid(Node element) {
         resetBorderColor(element);
     }
     // Method to set the field as invalid and display an error message
-    public static void setFieldInvalid(TextField field, Label errorLabel, String errorMessage) {
-        // Set the field border color to red to indicate error
-        setRedBorder(field);
+    public static void addErrorMessage(Label errorLabel, String errorMessage) {
         // Only update the error label if the error message is not null or empty
         if (errorLabel != null && errorMessage != null && !errorMessage.isEmpty()) {
             // append the new error message
@@ -29,8 +24,5 @@ public class FieldsServices {
             errorLabel.setText(old + "* " + errorMessage + "\n");
             errorLabel.setVisible(true);  // Make sure the error label is visible
         }
-    }
-    public static void setFieldInvalid(TextField field) {
-        setFieldInvalid(field, null, null);
     }
 }
