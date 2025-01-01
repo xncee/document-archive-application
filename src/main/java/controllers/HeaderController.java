c941441cfdad367000e0d0b58a547b9bac3a2d48
Saif <ro1c@yahoo.com>
2025-01-01 19:16:09 +0300

    added language selection section

--- src/main/java/controllers/HeaderController.java
+++ src/main/java/controllers/HeaderController.java
@@ -2,36 +2,49 @@ package controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import models.login.Login;
 
+import java.awt.event.ActionEvent;
+
 public class HeaderController {
     public VBox header;
-
+    @FXML
+    private ComboBox<String> languageComboBox;
     @FXML
     private Button notificationsButton;
-
     @FXML
     private Button userInfoButton;
-
     @FXML
     private Label loggedInUserLabel;
 
     @FXML
     public void initialize() {
+        prepareLanguages();
         String username = Login.getInstance().getUsername();
         //System.out.println(username);
         loggedInUserLabel.setText("@"+username);
     }
 
+    private void prepareLanguages() {
+        languageComboBox.getItems().addAll("ِArabic", "English");
+        languageComboBox.setValue("English"); // replace with preferred language
+    }
+
     @FXML
-    private void handleNotifications() {
+    public void handleLanguage() {
+        System.out.println("Language switched to "+languageComboBox.getValue());
+    }
+
+    @FXML
+    public void handleNotifications() {
 
     }
 
     @FXML
-    private void handleUserInfo() {
+    public void handleUserInfo() {
 
     }
 }
